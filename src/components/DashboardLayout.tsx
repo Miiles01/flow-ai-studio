8ad262771aa86c8fdd776d1952951a0090a719b0
Miiles Horton <miileshorton@MacBook-Pro-de-CLARO.local>
2026-05-13 16:26:18 -0600

    style: increase sidebar options spacing to 24px

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -97,7 +97,7 @@ function SidebarBody() {
         {/* Main navigation */}
         <SidebarGroup className="px-6">
           <SidebarGroupContent>
-            <SidebarMenu className="space-y-4">
+            <SidebarMenu className="space-y-6">
               {mainNav.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild className="h-auto p-0 hover:bg-transparent">
